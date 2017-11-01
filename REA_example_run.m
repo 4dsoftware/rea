3010eaf9656210e1d7df9e613472d5b15992eb00
razor06 <dudthu06@gmail.com>
2017-11-01 16:44:04 -0500

    Add files via upload

--- REA_example_run.m
+++ REA_example_run.m
@@ -12,6 +12,7 @@
 %(REA) is a tool to quantitatively determine combination effects including
 %synergy, additivity, and antagonism. 
 
+%Borisy, A.A. et al, PNAS 2003. doi: 10.1073/pnas.1337088100
 %Griner, L.A.M. et al, PNAS 2014. doi: 10.1073/pnas.1311846111
 
 %--------------------------------------------------------------------------
@@ -24,14 +25,16 @@ ft = 9; %font size
 lw = 3; %line width
 
 %% 2 read data file
-data = csvread('borisy_PNAS_2003.csv'); 
+data = csvread('Borisy_PNAS_2003.csv'); 
+%data = csvread('Griner_PNAS_2014.csv'); 
+
 %the data are formatted as the following:
 %column 1, concentrations of drug 1, in uM
 %column 2, concentrations of drug 2, in uM
 %column 3, corresponding survival rates in %
 %if the data show affected ratio or drug response instead, then the 
 %following step is needed
-data(:,3) = 100-data(:,3); 
+%data(:,3) = 100-data(:,3); 
 
 %% 3 perform the analysis
 f = REA_package(data,trim,ft,lw,'pentamidine','chlorpromazine',1); 
